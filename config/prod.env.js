5ac6e4268450f1bc6b14da2dd1170af360a742e4
Tom <tmelliottjr@gmail.com>
2018-04-23 12:42:07 -0400

    correct merging issues

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,8 @@
 'use strict'
-module.exports = {
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
   NODE_ENV: '"production"',
-  API_URL: '"http://api.nqd.tmelliottjr.com"'
-}
+  API_URL: '"http://api.nqd.tmelliottjr.com"',
+})
